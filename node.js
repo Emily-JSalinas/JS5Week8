c01942c9ab587de36cc0b73910268a8c5f9fbc50
burro011 <97425024+burro011@users.noreply.github.com>
2025-01-11 00:23:30 -0600

    Restarted project with different subject and added .jason and .gitignore files

--- node.js
+++ node.js
@@ -1,145 +1,161 @@
-class Animal {//class that will be the base of each input
-	constructor (name, age, species) {
+class ItemForRegistry {
+/**
+ * Will create an instance of an item
+ * @param {String} name The name of the item that will be added
+ * @param {Number} price The price of the item that will be added
+ */
+
+constructor(name, price) {
 		this.name = name;
-		this.age = age;
-		this.species = species;
+		this.price = price;
 	}
-	describe() { 
-		return `${this.name} is ${this.age} year(s) of age and is a ${this.species}`;
-	} 
+
+	itemSummary() {
+		return `You have added ${ this.name } to your registry and it costs $${ this.price.toFixed(2) }`;//added toFixed(2) to get code to show 2 decimal places for the price
+	}
 }
-class SpeciesType {
-	constructor(name){
-		this.name = name;
-		this.animalsAvailable = [];
+
+class Registry {
+	/**
+	 * Will create an instance of a Registry
+	 * @param {String} eventName The event name that the registry is being created for
+	 * @param {Date} dateOfEvent The date the event is to be held on
+	 * @param {String} registryOwner The person who is making the registry
+	 */
+	
+	constructor (registryOwner, eventName, dateOfEvent){
+		this.registryOwner = registryOwner;
+		this.eventName = eventName;
+		this.dateOfEvent = dateOfEvent;
+		this.listOfItems = [];
 	}
-	addAnimal(animal){
-		if (animal instanceof Animal) {
-			this.animalsAvailable.push(animal);
-		} else {
-		throw new Error(`Please input an animal's correct information. ${animal} does not meet the criteria.`);
-		 ;
+
+	addItem(item){
+			if (item instanceof ItemForRegistry) {
+				this.listOfItems.push(new ItemForRegistry);
+			} else {
+				return ('Error. Please add a valid item');
+			}
 		}
 	}
-		describe () {
-			return `There are ${this.animalsAvailable.length} avalable in the category of ${this.name}`
-		}
-}
+
+	registrySummary(){
+		return `Thank you, ${ registryOwner } for creating`
+	}
+
 class Menu {
-	constructor() {
-		this.animalCategory = [];//this would be the list of teams created
-		this.selectedSpecies = null; //This is set equal to null because no species has been selected yet
+	/**
+	 * Will create the methods in which the user can use/view a menu to manage the different registries 
+	 */
+	constructor(){
+		this.registries = []; //an array for the list of registries
+		this.currentRegistry = null; //there isn't a selected registry currently, but setting it to "null" will make it so the user can deal with one registry at a time
+	}
+
+	mainMenuOptions() {
+		let selection = this.mainMenu;
+		while (selection != 0){
+			switch (selection) {
+				case '1':
+					this.createRegistry();
+					break;
+				case '2':
+					this.registryMenu();
+					break;
+				case '3':
+					this.deleteRegistry();
+					break;
+				default:
+					selection = 0;
+			}
+			selection = this.mainMenu();
+		}
+		alert('Thank you. Goodbye!');
+	}
+
+	mainMenu(){ //Had to delete the tabs because it was showing arrows on the page that I didn't want
+		return prompt (`
+1) Create a registry
+2) View and edit a registry
+3) Delete a registry
+___________________________________________________
+
+To exit, press 0`);
+	}
+
+	registryMenuOptions() {
+		let secondSelection = this.registryMenu;
+		while (secondSelection > 0 && secondSelection <= 2){
+			switch (secondSelection) {
+				case '1':
+					this.viewRegistry();
+					break;
+				case '2':
+					this.editRegistry();
+					break;
+				default:
+					secondSelection = this.mainMenuOptions();
+					break;
+			}
+			secondSelection = this.registryMenu();
+		}
+		if (secondSelection === 0) {
+			alert ('Thank you! Goodbye');
+		} else {
+			this.mainMenu();
+		}
+		
+	}
+
+	registryMenu(){
+		return prompt (`
+1) View a registry
+2) Edit a registry
+_____________________________________________________
+
+To return to the main menu, press 3-9
+To exit, press 0`);
+	}
+
+	createItem() {
+		let name = prompt('Please enter the item you would like to add: ');
+		let price = prompt('Please enter the price of that item: ');
+		this.currentRegistry.addItem(new ItemForRegistry(name, price));
+	}
+
+	deleteItem() {
+		let itemNumber = prompt ('Which item number would you like to delete?');
+		if (itemNumber > -1 && itemNumber < this.currentRegistry.length) {
+			this.currentRegistry.splice(itemNumber);
+		} else {
+			throw new Error("Please enter an existing item number");
+		}
+	}
+
+	createRegistry() {
+		let registryOwner = prompt ('Please enter your name: ');
+		let eventName = prompt ('Please enter the name of your event (Birth, Wedding, etc.): ');
+		let dateOfEvent = prompt ('Please enter the date of your event (dd/mm/yyyy): ');
+		this.registries.push(new Registry(registryOwner, eventName, dateOfEvent));
+	}
+
+	viewRegistry(){
+
+	}
+
+	editRegistry() {
+
+		}
+
+	deleteRegistry() {
+		let registryNumber = prompt ('Please enter the number of the registry you would like to delete: ');
+		if (registryNumber > -1 && registryNumber < this.registries.length) {
+			this.registries
+		} else {
+			throw new Error(`Please enter an existing registry number. ${ registryNumber } has not been created yet`);
+		}
 	}
 }
 
-
-	//class Menu { // what drives the application and our choices
-	//constructor() {
-	//this.teams = [];
-	//this.selectedTeam = null; // manage one team at a time
-	//}
-	
-	//start() { // entry point to application
-	//let selection = this.showMainMenuOptions(); 
-	//while (selection != 0) {
-	//switch(selection) {
-	//case '1' :
-	//this.createTeam();
-	//break;
-	//case '2' :
-	//this.viewTeam();
-	//break;
-	//case '3' :
-	//this.deleteTeam();
-	//break;
-	//case '4' :
-	//this.displayTeams();
-	//break;
-	//default:
-	//selection = 0;
-	//}
-	//selection = this.showMainMenuOptions();
-	//}
-	//alert('Goodbye!');
-	//}
-	
-	
-	//showMainMenuOptions() {
-	//return prompt(`
-	//0) exit
-	//1) create a new team
-	//2) view a team
-	//3) delete a team
-	//4) display all teams
-	//`);
-//}
-	
-	//showTeamMenuOptions(teamInfo) {
-	//return prompt(`
-	//0) back
-	//1) add a new player
-	//2) delete a player
-	//-----------------
-	//${teamInfo}
-	//`);
-//	}
-	
-	//displayTeams() {
-	//let teamString = '';
-	//for (let i = 0; i < this.teams.length; i++) {
-	//teamString += i+ ') ' + this.teams[i].name + '\n';
-	//}
-	//alert(teamString);
-	//}
-	
-	//createTeam() {
-	//let name = prompt('Enter name for new team: ');
-	//this.teams.push(new Team(name));
-	//}
-	
-	//viewTeam() {
-	//let index = prompt("Enter the index of the team that you want to view:");
-	//if (index > -1 && index < this.teams.length) {
-	//this.selectedTeam = this.teams[index];
-	//let description = 'Team Name: ' + this.selectedTeam.name + '\n';
-	//description += ' ' + this.selectedTeam.describe() + '\n ';
-	//for (let i = 0; i < this.selectedTeam.players.length; i++) {
-	// description += i + ') ' + this.selectedTeam.players[i].name + ' - '
-	// + this.selectedTeam.players[i].position + '\n';
-	//description += i + ') ' + this.selectedTeam.players[i].describe() + '\n';
-	//}
-	//let selection1 = this.showTeamMenuOptions(description);
-	//switch (selection1) {
-	//case '1' :
-	//this.createPlayer();
-	//break;
-	//case '2' :
-	//this.deletePlayer();
-	//}
-	//} // validate user input
-	//}
-	
-	//deleteTeam() {
-	//let index = prompt('Enter the index of the team that you wish to delete: ');
-	//if (index > -1 && index < this.teams.length) {
-	//this.teams.splice(index,1);
-//	}
-	//}
-	
-	
-	//createPlayer() {
-	//let name = prompt('Enter name for new player: ');
-	//let position = prompt('Enter position for new player: ');
-	//this.selectedTeam.players.push(new Player(name, position));
-	//this.selectedTeam.addPlayer(new Player(name,position));
-	//}
-	
-	//deletePlayer() {
-	//let index = prompt('Enter the index of the player that you wish to delete: ');
-	//if (index > -1 && index < this.selectedTeam.players.length) { this.selectedTeam.players.splice(index,1);
-	//}
-	//}
-//	}
-	//let menu = new Menu();
-	//menu.start();
-	
+let start = new Menu;
+start.mainMenuOptions();
